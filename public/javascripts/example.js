d9117f7e365d2a12a8ddafbfbbe5808bd4549ad7
kzn17qr <kzn17qr@gmail.com>
2015-11-08 04:35:53 +0000

    Finished tutorial20

--- public/javascripts/example.js
+++ public/javascripts/example.js
@@ -3,6 +3,7 @@
  *
  */
 
+// CommentBox
 var CommentBox = React.createClass({
   loadCommentsFromServer: function() {
     $.ajax({
@@ -13,12 +14,31 @@ var CommentBox = React.createClass({
         this.setState({ data: data });
       }.bind(this),
       error: function(xhr, status, err) {
-        console.err(this.props.url, status, err.toString());
+        console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
   },
   getInitialState: function() {
     return { data: [] };
+  },
+  handleCommentSubmit: function(cmt) {
+//    var comments = this.state.data;
+//    var newComments = comments.concat([cmt]);
+//    this.setState({ data: newComments });
+
+    // submit to the server and refresh the list
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: cmt,
+      success: function(data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
   componentDidMount: function() {
     this.loadCommentsFromServer();
@@ -29,12 +49,13 @@ var CommentBox = React.createClass({
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={ this.state.data } />
-        <CommentForm />
+        <CommentForm onCommentSubmit={ this.handleCommentSubmit } />
       </div>
     );
   }
 });
 
+// CommentList
 var CommentList = React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function(cmt) {
@@ -53,17 +74,24 @@ var CommentList = React.createClass({
   }
 });
 
+// CommentForm
 var CommentForm = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
+
     var author = this.refs.author.value.trim();
     var text = this.refs.text.value.trim();
+
     if(!text || !author) {
       return;
     }
-    // TODO: send request to the server
+
+    // send request to the server
+    this.props.onCommentSubmit({ author: author, text: text });
+
     this.refs.author.value = '';
     this.refs.text.value = '';
+
     return;
   },
   render: function() {
@@ -77,9 +105,11 @@ var CommentForm = React.createClass({
   }
 });
 
+// Comment
 var Comment = React.createClass({
   render: function() {
     var rawMarkup = marked(this.props.children.toString(), { sanitize: true });
+
     return (
       <div className="comment">
         <h2 className="commentAuthor">
@@ -91,6 +121,7 @@ var Comment = React.createClass({
   }
 });
 
+// DOM
 ReactDOM.render(
   <CommentBox url="/api/comments" pollInterval={ 2000 } />,
   document.getElementById('content')
--- routes/api/comments.js
+++ routes/api/comments.js
@@ -1,12 +1,25 @@
 var fs = require('fs');
 var express = require('express');
 var router = express.Router();
+var store = 'db/comments.json';
 
 /* GET comments listing. */
 router.get('/', function(req, res, next) {
-  fs.readFile('db/comments.json', function(err, data) {
+  fs.readFile(store, function(err, data) {
     res.setHeader('Cache-Control', 'no-cache');
     res.json(JSON.parse(data));
+  });
+});
+
+/* POST comment creating */
+router.post('/', function(req, res) {
+  fs.readFile(store, function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile(store, JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(comments);
+    });
   });
 });
 
